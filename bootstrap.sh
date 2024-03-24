ee41a8495e820092fb18b9aa40680a0c461c06bf
Envixty <114357508+redkittty@users.noreply.github.com>
2024-03-23 17:43:10 -0700

    Updated bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,15 +2,13 @@
 
 cd "$(dirname "${BASH_SOURCE}")";
 
-git pull origin main;
-
 function doIt() {
 	rsync --exclude ".git/" \
 		--exclude ".DS_Store" \
 		--exclude ".osx" \
 		--exclude "bootstrap.sh" \
 		--exclude "README.md" \
-		--exclude "LICENSE-MIT.txt" \
+		--exclude "LICENSE" \
 		-avh --no-perms . ~;
 	source ~/.bash_profile;
 }
@@ -23,6 +21,11 @@ else
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
 		doIt;
 		echo "Dotfiles Copied Successful"
+		read -p "Do you also want to run the program installer script? (y/n) " -n 1
+        echo ""
+        if [[ $REPLY =~ ^[Yy]$ ]]; then
+        bash ~/.scripts/programs.sh
+fi
 	fi;
 fi;
 unset doIt;
