1ea74dede62c72ec2da7693c0cc40a33dc31d993
Envixty <114357508+redkittty@users.noreply.github.com>
2024-03-24 16:49:08 -0400

    Remade bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,31 +1,37 @@
 #!/bin/bash
 
-cd "$(dirname "${BASH_SOURCE}")";
+# Dotfiles directory (current directory)
+dotfiles_dir="./"
 
-function doIt() {
-	rsync --exclude ".git/" \
-		--exclude ".DS_Store" \
-		--exclude ".osx" \
-		--exclude "bootstrap.sh" \
-		--exclude "README.md" \
-		--exclude "LICENSE" \
-		-avh --no-perms . ~;
-	source ~/.bash_profile;
-}
+# Blacklist of files and directories to exclude from copying
+blacklist=(
+    .git
+    .gitignore
+    .DS_Store
+    bootstrap.sh
+    # Add more files and directories to exclude as needed
+)
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
-else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-		echo "Dotfiles Copied Successful"
-		read -p "Do you also want to run the program installer script? (y/n) " -n 1
-        echo ""
-        if [[ $REPLY =~ ^[Yy]$ ]]; then
-        bash ~/.scripts/programs.sh
-fi
-	fi;
-fi;
-unset doIt;
+# Copy all files and directories except those in the blacklist
+shopt -s dotglob # Include hidden files in the globbing
+for item in "$dotfiles_dir"*; do
+    # Extract basename of item
+    item_basename=$(basename "$item")
+
+    # Check if the item is not in the blacklist
+    if ! [[ " ${blacklist[@]} " =~ " ${item_basename} " ]]; then
+        # If it's a directory, recursively copy it
+        if [ -d "$item" ]; then
+            cp -r "$item" "$HOME/"
+        else
+            cp "$item" "$HOME/"
+        fi
+        echo "Copied $item_basename to $HOME/"
+    else
+        echo "$item_basename is in the blacklist and will not be copied."
+    fi
+done
+
+echo "Copy process complete."
+echo "Now installing Programs"
+source ~/.scripts/programs.sh
