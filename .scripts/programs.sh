5396319a781204129d530024bb7b5d80e1830cf8
Envixty <114357508+redkittty@users.noreply.github.com>
2024-03-23 20:40:54 -0400

    Added an automatic program installer

---
+++ .scripts/programs.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+# Distro selection prompt
+PS3="Select your Linux distribution: "
+options=("Debian/Ubuntu" "Fedora" "Arch/Manjaro" "None")
+select distro in "${options[@]}"; do
+  case $distro in
+    "Debian/Ubuntu")
+      DISTRO="DEBIAN_UBUNTU"
+      sudo apt update -y && sudo apt install -y zsh git steam vlc rust rust-analyzer ninja-build gettext cmake unzip curl build-essential python3
+      # Neovim
+      git clone https://github.com/neovim/neovim && cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo && sudo make install 
+      chsh -s /bin/zsh
+      break
+      ;;
+    "Fedora")
+      DISTRO="FEDORA"
+      sudo dnf update -y && sudo dnf install -y zsh git steam vlc rust rust-analyzer neovim python3
+      chsh -s /bin/zsh
+      break
+      ;;
+    "Arch/Manjaro")
+      DISTRO="ARCH"  # Assuming Manjaro uses Arch package manager (pacman)
+      sudo pacman -Syy zsh git steam vlc rust rust-analyzer neovim python3
+      chsh -s /bin/zsh
+      break
+      ;;
+    "None")
+      exit 0
+      ;;
+    *)
+      echo "Invalid choice."
+      ;;
+  esac
+done
