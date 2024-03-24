5989f7d21db06ffe00ed0fca9c4e183fcce9013d
Envixty <114357508+redkittty@users.noreply.github.com>
2024-03-23 21:29:51 -0400

    Update programs.sh

--- .scripts/programs.sh
+++ .scripts/programs.sh
@@ -6,7 +6,7 @@ select distro in "${options[@]}"; do
   case $distro in
     "Debian/Ubuntu")
       DISTRO="DEBIAN_UBUNTU"
-      sudo apt update -y && sudo apt install -y zsh git steam vlc rust rust-analyzer ninja-build gettext cmake unzip curl build-essential python3 alacritty
+      sudo apt update -y && sudo apt install -y zsh git steam vlc rust rust-analyzer ninja-build gettext cmake unzip curl build-essential python3 alacritty qbittorrent
       # Neovim
       git clone https://github.com/neovim/neovim && cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo && sudo make install 
       chsh -s /bin/zsh
@@ -14,13 +14,13 @@ select distro in "${options[@]}"; do
       ;;
     "Fedora")
       DISTRO="FEDORA"
-      sudo dnf update -y && sudo dnf install -y zsh git steam vlc rust rust-analyzer neovim python3 alacritty
+      sudo dnf update -y && sudo dnf install -y zsh git steam vlc rust rust-analyzer neovim python3 alacritty qbittorrent
       chsh -s /bin/zsh
       break
       ;;
     "Arch/Manjaro")
       DISTRO="ARCH"  # Assuming Manjaro uses Arch package manager (pacman)
-      sudo pacman -Syy zsh git steam vlc rust rust-analyzer neovim python3 alacritty
+      sudo pacman -Syy zsh git steam vlc rust rust-analyzer neovim python3 alacritty qbittorrent
       chsh -s /bin/zsh
       break
       ;;
