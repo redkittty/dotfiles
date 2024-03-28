de4f7acf554b4140c01e0892cc2d0de65fd97bc7
Envixty <114357508+redkittty@users.noreply.github.com>
2024-03-28 14:10:49 -0400

    Add files via upload

---
+++ .config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,23 @@
+local config = function()
+  local theme = require("lualine.themes.gruvbox")
+  theme.normal.c.bg = nil
+
+  require('lualine').setup {
+    options = {
+      theme = gruvbox,
+      globalstatus = true,
+    },
+    sections = {
+      lualine_a = {
+        {
+          'buffers',
+        },
+      },
+    },
+  }
+end
+return {
+  "nvim-lualine/lualine.nvim",
+  lazy = false,
+  config = config,
+}
