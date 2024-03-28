4e19b4b907375f34784c9442de7839aaef676a8f
Envixty <114357508+redkittty@users.noreply.github.com>
2024-03-28 14:09:39 -0400

    Update init.lua

--- .config/nvim/lua/config/init.lua
+++ .config/nvim/lua/config/init.lua
@@ -13,3 +13,30 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup('plugins')
+
+-- Options
+-- Tab / Indentation
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.softtabstop = 2
+opt.expandtab = true
+opt.smartindent = true
+opt.wrap = false
+
+-- Search
+opt.incsearch = true
+opt.ignorecase = true
+opt.smartcase = true
+opt.hlsearch = false
+
+-- Appearance
+opt.number = true
+opt.relativenumber = false
+opt.colorcolumn = '188'
+opt.completeopt = "menuone,noselect"
+
+-- Behaviour
+opt.hidden = true
+opt.errorbells = false
+opt.swapfile = false
+opt.backup = false
