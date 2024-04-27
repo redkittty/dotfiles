37c3d9d69d4eac4f67d737136e4295e9efa73f2f
redkittty <114357508+redkittty@users.noreply.github.com>
2024-04-27 18:40:23 -0400

    Removed Debian and Fedora from programs.sh
    - Removed options for Debian and Fedora in programs.sh due to these
      being my dotfiles and I am happy with Arch Linux, Also Debian and
      Fedora Package Lists in programs.sh were abandoned

--- .scripts/programs.sh
+++ .scripts/programs.sh
@@ -1,41 +1,11 @@
 #!/bin/sh
 # Distro selection prompt
-PS3="Select your Linux distribution: "
-options=("Debian/Ubuntu" "Fedora" "Arch/Manjaro" "None")
-echo "NOTE: DEBIAN AND FEDORA PACKAGE LISTS ARE ABANDONED"
-select distro in "${options[@]}"; do
-  case $distro in
-    "Debian/Ubuntu")
-      DISTRO="DEBIAN_UBUNTU"
-      sudo apt update -y && sudo apt install -y kitty 
-      sudo apt install -y zsh 
-      sudo apt install -y steam 
-      sudo apt install -y vlc 
-      sudo apt install -y rustc 
-      sudo apt install -y ninja-build 
-      sudo apt install -y gettext 
-      sudo apt install -y cmake 
-      sudo apt install -y unzip 
-      sudo apt install -y curl 
-      sudo apt install -y build-essential 
-      sudo apt install -y python3 
-      sudo apt install -y qbittorrent
-      # Neovim
-      git clone https://github.com/neovim/neovim && cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo && sudo make install 
-      chsh -s /bin/zsh
-      # Git login script
-      chmod +x ./gitsetup.sh
-      source ./gitsetup.sh
-      ;;
-    "Fedora")
-      DISTRO="FEDORA"
-      sudo dnf update -y && sudo dnf install -y kitty zsh git steam vlc rust rust-analyzer neovim python3 qbittorrent
-      chsh -s /bin/zsh
-      chmod +x ./gitsetup.sh
-      source ./gitsetup.sh
-      ;;
-    "Arch/Manjaro")
-      DISTRO="ARCH"  # Assuming Manjaro uses Arch package manager (pacman)
+echo "Would you like to install Additional Software: "
+options=("Yes" "No")
+select choice in "${options[@]}"; do
+  case $choice in
+    "Yes")
+      CHOICE="YES"  # Assuming Manjaro uses Arch package manager (pacman)
       sudo pacman -Syy pcmanfm htop zsh git steam vlc rust rust-analyzer neovim python3 kitty qbittorrent firefox bluez bluez-utils base-devel man-db ttf-font-awesome noto-fonts-emoji
       chsh -s /bin/zsh
       sudo systemctl start bluetooth.service
@@ -43,8 +13,9 @@ select distro in "${options[@]}"; do
       chmod +x ./archwm.sh
       source ./archwm.sh
       ;;
-    "None")
-      exit 0
+    "No")
+      chmod +x ./gitsetup.sh
+      source ./gitsetup.sh
       ;;
     *)
       echo "Invalid choice."
