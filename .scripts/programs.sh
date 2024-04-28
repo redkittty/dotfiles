2566018bbf5d8cc9755d344c95b76260022f2949
redkittty <114357508+redkittty@users.noreply.github.com>
2024-04-28 12:11:52 -0400

    Added Doas to my dotfiles
    Doas is a lightweight alternative to sudo
    - Added does to programs.sh
    - Added package management aliases to .zshrc
    - Added paru config

--- .scripts/programs.sh
+++ .scripts/programs.sh
@@ -6,7 +6,7 @@ select choice in "${options[@]}"; do
   case $choice in
     "Yes")
       CHOICE="YES"  # Assuming Manjaro uses Arch package manager (pacman)
-      sudo pacman -Syy pcmanfm htop zsh git steam mpv rust rust-analyzer neovim python3 kitty qbittorrent firefox bluez bluez-utils base-devel man-db ttf-font-awesome noto-fonts-emoji neofetch
+      sudo pacman -Syy doas pcmanfm htop zsh git steam mpv rust rust-analyzer neovim python3 kitty qbittorrent firefox bluez bluez-utils base-devel man-db ttf-font-awesome noto-fonts-emoji neofetch
       chsh -s /bin/zsh
       sudo systemctl start bluetooth.service
       sudo systemctl enable bluetooth.service
