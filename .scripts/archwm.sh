2e183a78c5088baafdb46eef9adef5a13023f9e6
redkittty <114357508+redkittty@users.noreply.github.com>
2024-04-28 14:20:44 -0400

    Removed Bloat
    - Removed awesome-wm-widgets being bundled in, instead it is git cloned
      during setup

--- .scripts/archwm.sh
+++ .scripts/archwm.sh
@@ -15,6 +15,7 @@ select wm in "${options[@]}"; do
       sudo pacman -Syyu picom blueman rofi rofi-emoji rofi-calc
       # Xrandr Monitor Settings
       xrandr --output HDMI-1 --primary --mode 1920x1080 --rate 144.00 --output eDP-1 --mode 1920x1080 --rate 59.98 --left-of HDMI-1
+      git clone https://github.com/streetturtle/awesome-wm-widgets.git ~/.config/awesome/awesome-wm-widgets
       ;;
     "OtherX11")
       WM="OTHERX11"
