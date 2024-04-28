a62979ae05157b4feeb14ac6487fc8b49b63691a
redkittty <114357508+redkittty@users.noreply.github.com>
2024-04-28 11:29:48 -0400

    Fixed archwm.sh
    - Fixed syntax in archwm.sh so it actually works

--- .scripts/archwm.sh
+++ .scripts/archwm.sh
@@ -1,27 +1,28 @@
 #!/bin/sh
 # Arch WM Selection Script
-# Installs additional programs
+# Installs additional programs for a wm
 PS3="Select your WM/DE: "
 options=("Hyprland" "Awesome" "OtherX11" "Other")
 select wm in "${options[@]}"; do
-  case wm in
+  case $wm in
     "Hyprland")
-      WM = "HYPRLAND"
+      WM="HYPRLAND"
       sudo pacman -Syyu waybar hyprpaper wl-clipboard blueman
       git clone https://aur.archlinux.org/hyprpicker-git.git && cd hyprpicker-git && makepkg -si && cd ..
       ;;
     "Awesome")
-      WM = "AWESOME"
+      WM="AWESOME"
       sudo pacman -Syyu picom blueman rofi rofi-emoji rofi-calc
       # Xrandr Monitor Settings
       xrandr --output HDMI-1 --primary --mode 1920x1080 --rate 144.00 --output eDP-1 --mode 1920x1080 --rate 59.98 --left-of HDMI-1
       ;;
     "OtherX11")
-      WM = "OTHERX11"
+      WM="OTHERX11"
       xrandr --output HDMI-1 --primary --mode 1920x1080 --rate 144.00 --output eDP-1 --mode 1920x1080 --rate 59.98 --left-of HDMI-1
       ;;
     "Other")
-      WM = "OTHER"
+      WM="OTHER"
+      break
       ;;
     *)
       echo "Invalid Choice"
