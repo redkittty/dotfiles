9693b63690b596f9c3b2384c6a107b3af736a633
redkittty <114357508+redkittty@users.noreply.github.com>
2024-05-05 16:43:46 -0400

    Fixed Program installation scripts to avoid bloat
    - Removed flameshot from programs.sh due to bloat
    - Added flameshot to archwm.sh due to flameshot only being used in AwesomeWM
      config

--- .scripts/archwm.sh
+++ .scripts/archwm.sh
@@ -12,7 +12,7 @@ select wm in "${options[@]}"; do
       ;;
     "Awesome")
       WM="AWESOME"
-      sudo pacman -Syyu picom blueman rofi rofi-emoji rofi-calc
+      sudo pacman -Syyu picom blueman rofi rofi-emoji rofi-calc flameshot
       # Xrandr Monitor Settings
       xrandr --output HDMI-1 --primary --mode 1920x1080 --rate 144.00 --output eDP-1 --mode 1920x1080 --rate 59.98 --left-of HDMI-1
       git clone https://github.com/streetturtle/awesome-wm-widgets.git ~/.config/awesome/awesome-wm-widgets
