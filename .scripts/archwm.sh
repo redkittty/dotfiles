12c9590060efff0b768ae7d51396d48129aff9d6
redkittty <114357508+redkittty@users.noreply.github.com>
2024-05-08 18:37:58 -0400

    Added Doom Emacs Config
    - Added a Doom Emacs Config for syntax highlighting and Grvbox theme
    - Made the archwm.sh choice delete the other's config
      ex: if awesomewm then rm -rf ~/.config/hypr ~/.config/waybar

--- .scripts/archwm.sh
+++ .scripts/archwm.sh
@@ -9,6 +9,7 @@ select wm in "${options[@]}"; do
       WM="HYPRLAND"
       sudo pacman -Syyu waybar hyprpaper wl-clipboard blueman
       git clone https://aur.archlinux.org/hyprpicker-git.git && cd hyprpicker-git && makepkg -si && cd ..
+      rm -rf ~/.config/awesome
       ;;
     "Awesome")
       WM="AWESOME"
@@ -16,6 +17,8 @@ select wm in "${options[@]}"; do
       # Xrandr Monitor Settings
       xrandr --output HDMI-1 --primary --mode 1920x1080 --rate 144.00 --output eDP-1 --mode 1920x1080 --rate 59.98 --left-of HDMI-1
       git clone https://github.com/streetturtle/awesome-wm-widgets.git ~/.config/awesome/awesome-wm-widgets
+      rm -rf ~/.config/waybar ~/.config/hypr
+      ;;
       ;;
     "OtherX11")
       WM="OTHERX11"
