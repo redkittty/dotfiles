2f8ad7fdb9cc2d038608803907febdb2ae779e5e
redkittty <114357508+redkittty@users.noreply.github.com>
2024-05-14 16:21:40 -0400

    Cleaned up Doom Emacs config.el
    - Cleaned up comments
    - Added Envixty Ascii Art to top

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -1,12 +1,23 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+;;
+;; ██████████                         ███               █████
+;;░░███░░░░░█                        ░░░               ░░███
+;; ░███  █ ░  ████████   █████ █████ ████  █████ █████ ███████   █████ ████
+;; ░██████   ░░███░░███ ░░███ ░░███ ░░███ ░░███ ░░███ ░░░███░   ░░███ ░███
+;; ░███░░█    ░███ ░███  ░███  ░███  ░███  ░░░█████░    ░███     ░███ ░███
+;; ░███ ░   █ ░███ ░███  ░░███ ███   ░███   ███░░░███   ░███ ███ ░███ ░███
+;; ██████████ ████ █████  ░░█████    █████ █████ █████  ░░█████  ░░███████
+;;░░░░░░░░░░ ░░░░ ░░░░░    ░░░░░    ░░░░░ ░░░░░ ░░░░░    ░░░░░    ░░░░░███
+;;                                                                ███ ░███
+;;                                                               ░░██████
+;;                                                                ░░░░░░
+;;
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets. It is optional.
-;; (setq user-full-name "John Doe"
-;;       user-mail-address "john@doe.com")
+;; ENVIXTY'S DOOM EMACS CONFIGURATION
 
+;; Place your private configuration here! Remember, you do not need to run 'doom sync' after modifying this file!
+
+;;; DOC ;;;
 ;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
 ;; - `doom-font' -- the primary font to use
@@ -15,44 +26,6 @@
 ;;   presentations or streaming.
 ;; - `doom-symbol-font' -- for symbols
 ;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
-;;
-;; See 'C-h v doom-font' for documentation and more examples of what they
-;; accept. For example:
-;;
-;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
-;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
-;;
-;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
-;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
-;; refresh your font settings. If Emacs still can't find your font, it likely
-;; wasn't installed correctly. Font issues are rarely Doom issues!
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-;; doom-one if broken
-(setq doom-theme 'doom-gruvbox)
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
-
-;; Whenever you reconfigure a package, make sure to wrap your config in an
-;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
-;;
-;;   (after! PACKAGE
-;;     (setq x y))
-;;
-;; The exceptions to this rule:
-;;
-;;   - Setting file/directory variables (like `org-directory')
-;;   - Setting variables which explicitly tell you to set them before their
-;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
-;;   - Setting doom variables (which start with 'doom-' or '+').
 ;;
 ;; Here are some additional functions/macros that will help you configure Doom.
 ;;
@@ -64,16 +37,23 @@
 ;;   `require' or `use-package'.
 ;; - `map!' for binding new keys
 ;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
-;; etc).
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
 
-;;; Vterm ;;;
+;;; GENERAL ;;;
+;; THEME ;;
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+;; doom-one if broken
+
+(setq doom-theme 'doom-gruvbox)
+
+;; LINE NUMBERS ;;
+;; To disable set to nil, for relative, set to relative
+
+(setq display-line-numbers-type 1)
+
+
+;;; VTERM ;;;
 
 (setq shell-file-name "/bin/fish"
       vterm-max-scrollback 5000)
@@ -81,7 +61,7 @@
       :desc "Vterm popup toggle"     "v t" #'+vterm/toggle)
 
 ;;; ORG MODE ;;;
-;; General Org-mode
+;; GENERAL ;;
 
 (map! :leader
       :desc "Org babel tangle" "m B" #'org-babel-tangle)
@@ -114,7 +94,7 @@
              "DONE(d)"           ; Task has been completed
              "CANCELLED(c)" )))) ; Task has been cancelled
 
-;; Customize font size for Org mode headings
+;; CUSTOM ORG-MODE FONT SIZE HEADINGS ;;
 (custom-set-faces!
   '(org-level-1 :height 1.3 :foreground "#B16286")
   '(org-level-2 :height 1.2 :foreground "#8EC07C")
