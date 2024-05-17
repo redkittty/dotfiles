321ecfca2019dad2d3c29c2d9942142d7442f153
redkittty <114357508+redkittty@users.noreply.github.com>
2024-05-17 16:43:49 -0400

    Modified nvim-lspconfig:
    - Removed lua_language_server
    - Added pyright

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,7 +1,12 @@
 return {
   "neovim/nvim-lspconfig",
+  dependencies = {
+      -- Automatically install LSPs to stdpath for neovim
+      { 'williamboman/mason.nvim', config = true },
+      'williamboman/mason-lspconfig.nvim',
+  },
   config = function ()
     require"lspconfig".rust_analyzer.setup{}
-    require"lspconfig".lua_language_server.setup{}
+    require"lspconfig".pyright.setup{}
   end,
 }
