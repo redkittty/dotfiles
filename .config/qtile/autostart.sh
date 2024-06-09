89821d82c1932b2db7568062616754c59f688728
redkittty <114357508+redkittty@users.noreply.github.com>
2024-06-09 19:57:56 -0400

    Added qtile config
    - added qtile config
    - added new wallpaper

---
+++ .config/qtile/autostart.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+function run {
+  if ! pgrep -x $(basename $1 | head -c 15) 1>/dev/null;
+  then
+    $@&
+  fi
+}
+
+# Utilities
+nm-applet &
+flameshot &
+picom &
+conky &
+/usr/bin/emacs --daemon &
+xrandr --output HDMI-1 --primary --mode 1920x1080 --rate 144.00 --output eDP-1 --mode 1920x1080 --rate 59.98 --left-of HDMI-1 &
