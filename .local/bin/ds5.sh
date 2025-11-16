103d601c678d5289a15a6b3d0c2247ae3aa69780
redkittty <114357508+redkittty@users.noreply.github.com>
2025-11-16 13:54:49 -0500

    e

--- .local/bin/ds5.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Really basic script to make DualSense work by emulating a Xbox360 controller
-
-echo "Enter to make DualSense controller work as 360 controller"
-sudo hciconfig hciX up
-sudo ds4drv --emulate-xpad
