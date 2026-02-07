4801c71a9476c68ac720f321329dfb063cc8ff43
redkittty <114357508+redkittty@users.noreply.github.com>
2026-02-07 14:58:55 -0500

    Changed waybar bg color
    - Changed some shi

--- .config/hypr/modules/autostart.sh
+++ .config/hypr/modules/autostart.sh
@@ -9,10 +9,11 @@ exec nm-applet &
 exec swaync &
 exec hypridle &
 exec systemctl --user start hyprpolkitagent &
-exec hyprpm reload -nn &
+#exec hyprpm reload -nn &
 exec hyprsunset &
 exec swayosd-server &
 exec udiskie -a -T &
+exec dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP &
 
 # User Programs
 exec steam &
